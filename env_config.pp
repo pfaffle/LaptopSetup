1d6c2d8ca2361443373f8881091bf7f44e28d66e
Craig Meinschein <cmeinsch@gmail.com>
2015-05-27 22:56:26 -0700

    Ensure user account and profile creation.

--- env_config.pp
+++ env_config.pp
@@ -1,5 +1,19 @@
-$account = split($::id,'\\')
-$user = $account[1]
+$user = 'pfaffle'
+$realname = 'Craig Meinschein'
+$userprofile = ["C:/Users/$user/","C:/Users/$user/AppData/","C:/Users/$user/AppData/Roaming"]
+
+user { $user:
+	ensure     => present,
+	comment    => $realname,
+	groups     => ['Administrators','Users'],
+	managehome => true,
+	password   => 'Temppass1',
+}
+
+file { $userprofile:
+	ensure             => directory,
+	source_permissions => ignore,
+}
 
 windows_env { 'GIT_SSH':
 	ensure    => present,
