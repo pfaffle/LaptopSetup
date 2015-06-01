574db444e4533fa35dd1d103b2f213fe1218c00e
Craig Meinschein <cmeinsch@gmail.com>
2015-05-31 22:26:01 -0700

    Account for 64-bit and 32-bit shells.

--- go.cmd
+++ go.cmd
@@ -1,6 +1,19 @@
 @echo off
-set POWERSHELL64=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
-set POWERSHELL32=C:\Windows\SYSWOW64\WindowsPowerShell\v1.0\powershell.exe
+if exist "C:\Windows\Sysnative" (
+  set POWERSHELL_NATIVE=C:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe
+  set CMD_NATIVE=C:\Windows\Sysnative\cmd.exe
+) else (
+  set POWERSHELL_NATIVE=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+  set CMD_NATIVE=C:\Windows\System32\cmd.exe
+)
+if exist "C:\Windows\SysWOW64" (
+  set POWERSHELL_SYSWOW=C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe
+  set CMD_SYSWOW=C:\Windows\SysWOW64\cmd.exe
+) else (
+  set POWERSHELL_SYSWOW=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+  set CMD_SYSWOW=C:\Windows\System32\cmd.exe
+)
+
 @REM === Laptop setup START
 pushd %~dp0
 set dir=%~dp0
@@ -8,11 +21,11 @@ if exist z: net use /del z:
 echo Pushing \\localhost\%dir:~0,1%$\%dir:~3,-1%
 net use z: \\localhost\%dir:~0,1%$\%dir:~3,-1%
 
-%POWERSHELL64% -NoProfile Set-ExecutionPolicy Bypass -Force
-%POWERSHELL32% -NoProfile Set-ExecutionPolicy Bypass -Force
+%POWERSHELL_NATIVE% -NoProfile Set-ExecutionPolicy Bypass -Force
+%POWERSHELL_SYSWOW% -NoProfile Set-ExecutionPolicy Bypass -Force
 
 @REM === Chocolatey setup START
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+%POWERSHELL_NATIVE% -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 set ChocolateyInstall=%PROGRAMDATA%\Chocolatey
 set ChocolateyPath=%ChocolateyInstall%\bin
 echo %PATH% | findstr "%ChocolateyPath%" >nul 2>&1
@@ -20,30 +33,30 @@ if ERRORLEVEL 1 set PATH=%PATH%;%ChocolateyPath%
 @REM === Chocolatey setup END
 
 @REM === Puppet setup START
-choco install puppet -y
-set PuppetPath=%PROGRAMFILES%\Puppet Labs\Puppet\bin
+%CMD_NATIVE% /c choco install puppet -y
+set PuppetPath=C:\Program Files\Puppet Labs\Puppet\bin
 echo %PATH% | findstr "%PuppetPath%" >nul 2>&1
 if ERRORLEVEL 1 set PATH=%PATH%;%PuppetPath%
-cmd /c puppet module install chocolatey-chocolatey
-cmd /c puppet module install badgerious-windows_env
+%CMD_NATIVE% /c puppet module install chocolatey-chocolatey
+%CMD_NATIVE% /c puppet module install badgerious-windows_env
 @REM === Puppet setup END
 
 @REM === Setup packages and environment START
-cmd /c puppet apply software.pp
-cmd /c puppet apply env_config.pp
-@powershell -NoProfile -Command "&{Import-Module PsGet; Install-Module -Global PSReadline}"
+%CMD_NATIVE% /c puppet apply software.pp
+%CMD_NATIVE% /c puppet apply env_config.pp
+%POWERSHELL_NATIVE% -NoProfile -Command "&{Import-Module PsGet; Install-Module -Global PSReadline}"
 @REM === Setup packages and environment END
 
 @REM === Set pinned taskbar items START
 del /q "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\*"
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /va /f
+%CMD_NATIVE% /c reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband /va /f
 @REM this launches explorer as an administrator which makes all child processes run as admin as well
 @REM taskkill /im explorer.exe /f
 @REM start explorer.exe
-cscript pinitem.vbs /item:"%PROGRAMFILES(x86)%\Google\Chrome\Application\chrome.exe" /taskbar
-cscript pinitem.vbs /item:"%PROGRAMFILES(x86)%\Mozilla Firefox\firefox.exe" /taskbar
-cscript pinitem.vbs /item:"%PROGRAMFILES%\ConEmu\ConEmu64.exe" /taskbar
-cscript pinitem.vbs /item:"%PROGRAMFILES(x86)%\Mozilla Thunderbird\thunderbird.exe" /taskbar
+%CMD_NATIVE% /c cscript pinitem.vbs /item:"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" /taskbar
+%CMD_NATIVE% /c cscript pinitem.vbs /item:"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" /taskbar
+%CMD_NATIVE% /c cscript pinitem.vbs /item:"C:\Program Files\ConEmu\ConEmu64.exe" /taskbar
+%CMD_NATIVE% /c cscript pinitem.vbs /item:"C:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe" /taskbar
 @REM === Set pinned taskbar items END
 
 net use z: /del
